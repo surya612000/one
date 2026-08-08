d94912cabf32126e6fb6f99c163daddc0e5ee270
MUSTAFA SHAIK <106308110+devops0014@users.noreply.github.com>
2026-08-08 20:38:18 +0530

    Update dockerPush.groovy

--- vars/dockerPush.groovy
+++ vars/dockerPush.groovy
@@ -1,6 +1,6 @@
 def call (String image_name, String tag) {
     withDockerRegistry([credentialsId: 'dockerhub']) {
         echo "Pushing Docker image to Docker Hub..."
-         sh "docker push ${image_name}:${tag} ."
+         sh "docker push ${image_name}:${tag}"
     }
 }
